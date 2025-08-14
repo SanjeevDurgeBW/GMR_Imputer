3e006436e1d22bf5fc3bba4e84bc3d994e7b7ace
SanjeevDurgeBW <sdurge@borgwarner.com>
2025-08-14 13:41:22 -0400

    removed app.run

--- TamAPP.py
+++ TamAPP.py
@@ -480,8 +480,8 @@ def preprocess_input_data(df, drop_cols):
 #         sys.exit(1)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
 
 
 # import joblib
