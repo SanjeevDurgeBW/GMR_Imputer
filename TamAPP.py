621dce907be434f6e779e885e8f1986428906646
SanjeevDurgeBW <sdurge@borgwarner.com>
2025-08-26 14:17:32 -0400

    updated model name

--- TamAPP.py
+++ TamAPP.py
@@ -77,10 +77,10 @@ PRODUCT_MODEL_MAP = {
     #     "drop_cols": 23
     # },
     "cv_turbo": {
-        "model_path": os.path.join(MODEL_DIR, "cv_turbo_d8_model.pkl"),
-        "checksum_path": os.path.join(MODEL_DIR, "cv_turbo_d8_model.pkl.sha256"),
-        "blob_model": "cv_turbo_d8_model.pkl",
-        "blob_checksum": "cv_turbo_d8_model.pkl.sha256",
+        "model_path": os.path.join(MODEL_DIR, "cv_turbo_d8_new_model.pkl"),
+        "checksum_path": os.path.join(MODEL_DIR, "cv_turbo_d8_new_model.pkl.sha256"),
+        "blob_model": "cv_turbo_d8_new_model.pkl",
+        "blob_checksum": "cv_turbo_d8_new_model.pkl.sha256",
         "drop_cols": 23
     },
     "cv_egr": {
